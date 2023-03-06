74168da4ab89d3cb601e2392a8cf5235b22a559e
apjavier2 <apjavier27@gmail.com>
2023-03-06 13:22:56 +0800

    created detail page and css style

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+const MeetupDetails = () => {
+  return (
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg"
+      title="First Meetup"
+      description="The first meetup"
+      address="Somewhere City"
+    ></MeetupDetail>
+  );
+};
+
+export default MeetupDetails;
