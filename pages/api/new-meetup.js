f1d30e1f04a29758a930e95289a3ba328d1340ba
apjavier2 <apjavier27@gmail.com>
2023-03-06 14:44:49 +0800

    created new meetup POST api and connected it to mongodb

---
+++ pages/api/new-meetup.js
@@ -0,0 +1,20 @@
+// /api/new-meetup
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
+  if (req.method === "POST") {
+    const client = MongoClient.connect(
+      "mongodb+srv://apjavier27:3kDi3lhoaZ87fBLW@cluster0.s6lzivr.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+    const result = await meetupsCollection.insertOne(data);
+    console.log(result);
+
+    client.close();
+    res.status(201).json({ message: "Meetup inserted!" });
+  }
+}
+
+export default handler;
